99002a9ab0f2c69b7966304c837cf7430d930ed6
kwanmo2 <kwanmo2@gmail.com>
2022-01-17 20:01:11 +0900

    try to add own interp algorithm

--- Subpixel_registration.py
+++ Subpixel_registration.py
@@ -18,6 +18,19 @@ from tkinter import filedialog
 
 
 file_list =  filedialog.askopenfilenames(initialdir ="C://",title = "choose your dark images")
+
+def sinc_interp(x, s, u):
+    if len(x) != len(s):
+        raise ValueError('x and s must be the same length')
+    
+    # Find the period    
+    T = s[1] - s[0]
+    
+    sincM = np.tile(u, (len(s), 1)) - np.tile(s[:, np.newaxis], (1, len(u)))
+    y = np.dot(x, np.sinc(sincM/T))
+    return y
+
+"""
 NumberofImage = np.linspace(1,len(file_list),len(file_list),dtype=int)
 NumberofImage = len(NumberofImage)
 index=np.zeros((NumberofImage,5))
@@ -38,14 +51,17 @@ for i in range(0,NumberofImage):
 
 
     frame.loc[i] = [file_list[i],shifted[1],shifted[0],error,"N/A"]
-
-#FFT로 직접 계산
 """
+#FFT로 직접 계산
+
 src = cv2.imread(file_list[0],cv2.IMREAD_ANYDEPTH|cv2.IMREAD_ANYCOLOR)
 b,g,r = cv2.split(src)
 
 reference_IMG = r
+h, w = reference_IMG.shape
 Target_IMG = g
+reference_IMG = np.pad(reference_IMG,((100,100),(100,100)), 'constant', constant_values=0)
+Target_IMG = np.pad(Target_IMG,((100,100),(100,100)), 'constant', constant_values=0)
 
 S1 = np.fft.fftshift(np.fft.fft2(reference_IMG))
 S2 = np.fft.fftshift(np.fft.fft2(Target_IMG))
@@ -53,14 +69,18 @@ S2 = np.fft.fftshift(np.fft.fft2(Target_IMG))
 Q = (S1 * np.conj(S2)) / abs(S1*conj(S2))
 Qi = np.fft.ifft2(Q)
 
+Yp, Xp = np.where(abs(Qi) == np.max(abs(Qi)))
 
-deltaY, deltaX = np.where(abs(Qi) == np.max(abs(Qi)))
-"""
+Qi_real = abs(Qi)
 
+deltaX  = (Qi_real[1,0]*Xp + Qi_real[0,0]*1)/(Qi_real[1,0]+Qi_real[0,0]) -1
+deltaY  = (Qi_real[0,1]*Yp + Qi_real[0,0]*1)/(Qi_real[0,1]+Qi_real[0,0]) -1
+
+y = sinc_interp(np.array((0,1,2)), np.array((Qi_real[-1,0],Qi_real[0,0],Qi_real[1,0])), 1000)
 
 #참고용
 """
-#FFT shift로 이미지 shift하기 
+#FFT shift로 이미지 shift하기 .
 input_IMG = cv2.imread(file_list[0],cv2.IMREAD_ANYDEPTH);
 rows, cols = input_IMG.shape
 FFT_IMG = np.fft.fft2(input_IMG)
